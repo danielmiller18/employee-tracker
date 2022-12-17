966bcdcee3a1b92e00d526c7cf118dc8c9ad331c
Daniel Miller <104872554+danielmiller18@users.noreply.github.com>
2022-12-17 17:11:50 -0600

    updated server.js

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const validate = require('./javascript/validate');
 // Database Connect and Starter Title
 connection.connect((error) => {
   if (error) throw error;
-  console.log(chalk.yellow.bold(`====================================================================================`));
-  console.log(``);
-  console.log(chalk.yellowBright.bold(figlet.textSync('Employee Tracker By Daniel')));
-  console.log(``);
-  console.log(``);
-  console.log(chalk.yellow.bold(`====================================================================================`));
+ 
   promptUser();
 });
 
